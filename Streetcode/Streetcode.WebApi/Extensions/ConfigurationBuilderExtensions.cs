2daf33e544fa51828d53ce7a6151c21f4916aa5c
yarikko <kornienko.yaroslav07@gmail.com>
2025-02-20 10:20:43 +0200

    some bugfix

--- Streetcode/Streetcode.WebApi/Extensions/ConfigurationBuilderExtensions.cs
+++ Streetcode/Streetcode.WebApi/Extensions/ConfigurationBuilderExtensions.cs
@@ -7,6 +7,7 @@
             builder.SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddUserSecrets<Program>()
                 .AddEnvironmentVariables("STREETCODE_");
 
             return builder;

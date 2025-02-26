4a12de5140fe177f43688589f197265a29e43ce0
Volodymyr Kurylin <29712090+saynotoid@users.noreply.github.com>
2025-02-26 12:00:35 +0200

    Add 2 SendEmailHandler Tests (#31)

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/EmailTests/SendEmailHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using Moq;
+using Streetcode.BLL.MediatR.Email;
+using Streetcode.BLL.Interfaces.Email;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.DTO.Email;
+using Streetcode.DAL.Entities.AdditionalContent.Email;
+
+namespace Streetcode.XUnitTest.MediatRTests.EmailTests;
+
+public class SendEmailHandlerTests
+{
+    private readonly Mock<IEmailService> _emailServiceMock = new();
+    private readonly Mock<ILoggerService> _loggerMock = new();
+    private readonly SendEmailHandler _handler;
+
+    public SendEmailHandlerTests() =>
+        _handler = new(_emailServiceMock.Object, _loggerMock.Object);
+
+    [Fact]
+    public async Task Handle_ShouldReturnSuccess_WhenEmailSentSuccessfully()
+    {
+        _emailServiceMock.Setup(s => s.SendEmailAsync(It.IsAny<Message>()))
+            .ReturnsAsync(true);
+
+        var command = new SendEmailCommand(
+            new EmailDTO { From = "test@test.com", Content = "Test content" });
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        Assert.True(result.IsSuccess);
+        _emailServiceMock.Verify(
+            s => s.SendEmailAsync(It.IsAny<Message>()), Times.Once);
+        _loggerMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenEmailSendingFails()
+    {
+        _emailServiceMock.Setup(s => s.SendEmailAsync(It.IsAny<Message>()))
+            .ReturnsAsync(false);
+
+        var command = new SendEmailCommand(
+            new EmailDTO { From = "test@test.com", Content = "Test content" });
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        Assert.False(result.IsSuccess);
+        Assert.Single(result.Errors);
+        _emailServiceMock.Verify(
+            s => s.SendEmailAsync(It.IsAny<Message>()), Times.Once);
+        const string errorMsg = $"Failed to send email message";
+        _loggerMock.Verify(
+            l => l.LogError(command, errorMsg), Times.Once);
+    }
+}

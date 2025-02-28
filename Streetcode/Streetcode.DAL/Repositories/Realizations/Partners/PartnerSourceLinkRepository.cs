c5c1de226f06f3ae63f894f669b479c797c0c670
Alex Zaslavsky <alex.zaslavsky.ua@gmail.com>
2025-02-28 08:40:34 +0200

    refactoring

--- Streetcode/Streetcode.DAL/Repositories/Realizations/Partners/PartnerSourceLinkRepository.cs
+++ Streetcode/Streetcode.DAL/Repositories/Realizations/Partners/PartnerSourceLinkRepository.cs
@@ -3,13 +3,13 @@ using Streetcode.DAL.Persistence;
 using Streetcode.DAL.Repositories.Interfaces.Partners;
 using Streetcode.DAL.Repositories.Realizations.Base;
 
-namespace Streetcode.DAL.Repositories.Realizations.Partners
+namespace Streetcode.DAL.Repositories.Realizations.Partners;
+
+public class PartnerSourceLinkRepository
+    : RepositoryBase<PartnerSourceLink>, IPartnerSourceLinkRepository
 {
-    public class PartnerSourceLinkRepository : RepositoryBase<PartnerSourceLink>, IPartnerSourceLinkRepository
+    public PartnerSourceLinkRepository(StreetcodeDbContext context)
+        : base(context)
     {
-        public PartnerSourceLinkRepository(StreetcodeDbContext context)
-            : base(context)
-        {
-        }
     }
 }

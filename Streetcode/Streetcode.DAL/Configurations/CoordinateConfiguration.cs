17b227c95a7f5b4478230d01e06b23920da765a0
yarikko <kornienko.yaroslav07@gmail.com>
2025-03-03 12:18:34 +0200

    moved the configuration settings of modes to separate classes

---
+++ Streetcode/Streetcode.DAL/Configurations/CoordinateConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streetcode.DAL.Entities.AdditionalContent.Coordinates;
+using Streetcode.DAL.Entities.AdditionalContent.Coordinates.Types;
+using Streetcode.DAL.Entities.Toponyms;
+
+namespace Streetcode.DAL.Configurations
+{
+    public class CoordinateConfiguration : IEntityTypeConfiguration<Coordinate>
+    {
+        public void Configure(EntityTypeBuilder<Coordinate> builder)
+        {
+            builder
+                .HasDiscriminator<string>("CoordinateType")
+                .HasValue<Coordinate>("coordinate_base")
+                .HasValue<StreetcodeCoordinate>("coordinate_streetcode")
+                .HasValue<ToponymCoordinate>("coordinate_toponym");
+        }
+    }
+}
---
+++ Streetcode/Streetcode.DAL/Configurations/StreetcodeTagIndexConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streetcode.DAL.Entities.AdditionalContent;
+
+namespace Streetcode.DAL.Configurations
+{
+    public class StreetcodeTagIndexConfiguration : IEntityTypeConfiguration<StreetcodeTagIndex>
+    {
+        public void Configure(EntityTypeBuilder<StreetcodeTagIndex> builder)
+        {
+            builder
+                .HasKey(
+                nameof(StreetcodeTagIndex.StreetcodeId),
+                nameof(StreetcodeTagIndex.TagId));
+        }
+    }
+}

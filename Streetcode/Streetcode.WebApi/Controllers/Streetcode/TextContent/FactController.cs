18ee52c8f1bdf1edcb17377daf307bb6bfe55264
Liubomyr Krasivskyi <161932012+krasivskyi@users.noreply.github.com>
2025-03-10 15:24:53 +0200

    feat(facts): add update endpoint for facts in FactController

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
@@ -4,6 +4,7 @@ using Streetcode.BLL.MediatR.Streetcode.Fact.GetAll;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetById;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetByStreetcodeId;
 using Streetcode.BLL.MediatR.Streetcode.Fact.Reorder;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Update;
 
 namespace Streetcode.WebApi.Controllers.Streetcode.TextContent;
 
@@ -25,6 +26,12 @@ public class FactController : BaseApiController
     public async Task<IActionResult> GetByStreetcodeId([FromRoute] int streetcodeId)
     {
         return HandleResult(await Mediator.Send(new GetFactByStreetcodeIdQuery(streetcodeId)));
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] FactUpdateCreateDTO factDTO)
+    {
+        return HandleResult(await Mediator.Send(new UpdateFactCommand(factDTO)));
     }
 
     [HttpPut("reorder")]

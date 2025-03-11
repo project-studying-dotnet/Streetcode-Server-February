9ad4be4ab50ef2b5b089fd68f3f31f92e1e5c50d
Eugene Sokolov <sokolow054@gmail.com>
2025-03-11 19:36:01 +0200

    Added tests for AdditionalContetnt/Tag/GetByStreetcodeId

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/AdditionalContent/Tag/GetByStreetcodeId/GetTagByStreetcodeIdHandlerTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using Moq;
+using Xunit;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Query;
+using Streetcode.BLL.DTO.AdditionalContent.Tag;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.AdditionalContent.Tag.GetByStreetcodeId;
+using Streetcode.DAL.Entities.AdditionalContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using TagEntity = Streetcode.DAL.Entities.AdditionalContent.Tag;
+
+namespace Streetcode.XUnitTest.MediatRTests
+    .AdditionalContent.Tag.GetByStreetcodeId;
+
+public class GetTagByStreetcodeIdHandlerTests
+{
+    private readonly Mock<IRepositoryWrapper> _repositoryWrapperMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly Mock<ILoggerService> _loggerMock;
+    private readonly GetTagByStreetcodeIdHandler _handler;
+
+    public GetTagByStreetcodeIdHandlerTests()
+    {
+        _repositoryWrapperMock = new Mock<IRepositoryWrapper>();
+        _mapperMock = new Mock<IMapper>();
+        _loggerMock = new Mock<ILoggerService>();
+
+        _handler = new GetTagByStreetcodeIdHandler(
+            _repositoryWrapperMock.Object,
+            _mapperMock.Object,
+            _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnError_WhenTagsNotFound()
+    {
+        // Arrange
+        var query = new GetTagByStreetcodeIdQuery(1);
+        _repositoryWrapperMock
+            .Setup(repo => repo.StreetcodeTagIndexRepository
+            .GetAllAsync(
+                It.IsAny<Expression<Func<StreetcodeTagIndex, bool>>>(),
+                It.IsAny<Func<IQueryable<StreetcodeTagIndex>,
+                IIncludableQueryable<StreetcodeTagIndex, object>>?>()))
+            .ReturnsAsync((IEnumerable<StreetcodeTagIndex>)null!);
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldLogError_WhenTagsNotFound()
+    {
+        // Arrange
+        var query = new GetTagByStreetcodeIdQuery(1);
+        _repositoryWrapperMock
+            .Setup(repo => repo.StreetcodeTagIndexRepository
+            .GetAllAsync(
+                It.IsAny<Expression<Func<StreetcodeTagIndex, bool>>>(),
+                It.IsAny<Func<IQueryable<StreetcodeTagIndex>,
+                IIncludableQueryable<StreetcodeTagIndex, object>>?>()))
+            .ReturnsAsync((IEnumerable<StreetcodeTagIndex>)null!);
+
+        // Act
+        await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        _loggerMock.Verify(
+            logger => logger.LogError(
+                It.IsAny<object>(), It.IsAny<string>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnOrderedTags_WhenTagsFound()
+    {
+        // Arrange
+        var query = new GetTagByStreetcodeIdQuery(1);
+        var tags = new List<StreetcodeTagIndex>
+        {
+            new() { Index = 2, Tag = new TagEntity { Title = "TestTag" } },
+            new() { Index = 1, Tag = new TagEntity { Title = "TestTag" } }
+        };
+
+        _repositoryWrapperMock
+            .Setup(repo => repo.StreetcodeTagIndexRepository
+            .GetAllAsync(
+                It.IsAny<Expression<Func<StreetcodeTagIndex, bool>>>(),
+                It.IsAny<Func<IQueryable<StreetcodeTagIndex>,
+                IIncludableQueryable<StreetcodeTagIndex, object>>?>()))
+            .ReturnsAsync(tags);
+
+        _mapperMock.Setup(mapper => mapper
+            .Map<IEnumerable<StreetcodeTagDTO>>(
+                It.IsAny<IEnumerable<StreetcodeTagIndex>>()))
+            .Returns(new List<StreetcodeTagDTO>());
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+    }
+}

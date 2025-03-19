ea9b55ea631f33c4dd2f19b458689747c5d1c8ae
Eugene Sokolov <82029018+SkiTee@users.noreply.github.com>
2025-03-19 09:17:11 +0200

    142 add unit tests for mediatrteam (#146)
    * Added tests for MediatR/Team/GetAll
    
    * Added tests for MediatR/Team/GetAllMain + fixes
    
    * Added tests for MediatR/Team/GetById

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Team/GetAll/GetAllTeamHandlerTests.cs
@@ -0,0 +1,102 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using Moq;
+using Xunit;
+using Microsoft.EntityFrameworkCore.Query;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Team.GetAll;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.DAL.Entities.Team;
+
+namespace Streetcode.XUnitTest.MediatRTests.Team.GetAll;
+
+public class GetAllTeamHandlerTests
+{
+    private readonly Mock<IRepositoryWrapper> _repositoryWrapperMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly Mock<ILoggerService> _loggerMock;
+    private readonly GetAllTeamHandler _handler;
+
+    public GetAllTeamHandlerTests()
+    {
+        _repositoryWrapperMock = new Mock<IRepositoryWrapper>();
+        _mapperMock = new Mock<IMapper>();
+        _loggerMock = new Mock<ILoggerService>();
+        _handler = new GetAllTeamHandler(
+            _repositoryWrapperMock.Object,
+            _mapperMock.Object,
+            _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailResult_WhenNoTeamFound()
+    {
+        _repositoryWrapperMock
+        .Setup(r => r.TeamRepository.GetAllAsync(
+        It.IsAny<Expression<Func<TeamMember, bool>>>(),
+        It.IsAny<Func<IQueryable<TeamMember>,
+        IIncludableQueryable<TeamMember, object>>>()))
+        .ReturnsAsync((IEnumerable<TeamMember>)null!);
+
+        var result = await _handler.Handle(
+            new GetAllTeamQuery(), CancellationToken.None);
+
+        Assert.True(result.IsFailed);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldLogError_WhenNoTeamFound()
+    {
+        _repositoryWrapperMock
+        .Setup(r => r.TeamRepository.GetAllAsync(
+        It.IsAny<Expression<Func<TeamMember, bool>>>(),
+        It.IsAny<Func<IQueryable<TeamMember>,
+        IIncludableQueryable<TeamMember, object>>>()))
+        .ReturnsAsync((IEnumerable<TeamMember>)null!);
+
+        await _handler.Handle(new GetAllTeamQuery(), CancellationToken.None);
+
+        _loggerMock.Verify(
+            l => l.LogError(
+                It.IsAny<GetAllTeamQuery>(), It.Is<string>(
+                    msg => msg.Contains("Cannot find any team"))), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnOkResult_WhenTeamFound()
+    {
+        var teams = new List<TeamMember> { new() };
+        _repositoryWrapperMock
+        .Setup(r => r.TeamRepository.GetAllAsync(
+        It.IsAny<Expression<Func<TeamMember, bool>>>(),
+        It.IsAny<Func<IQueryable<TeamMember>,
+        IIncludableQueryable<TeamMember, object>>>()))
+        .ReturnsAsync(teams);
+
+        _mapperMock
+            .Setup(m => m.Map<IEnumerable<TeamMemberDTO>>(teams))
+            .Returns(new List<TeamMemberDTO>());
+
+        var result = await _handler.Handle(
+            new GetAllTeamQuery(), CancellationToken.None);
+
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldMapTeams_WhenTeamFound()
+    {
+        var teams = new List<TeamMember> { new() };
+        _repositoryWrapperMock
+        .Setup(r => r.TeamRepository.GetAllAsync(
+        It.IsAny<Expression<Func<TeamMember, bool>>>(),
+        It.IsAny<Func<IQueryable<TeamMember>,
+        IIncludableQueryable<TeamMember, object>>>()))
+        .ReturnsAsync(teams);
+        await _handler.Handle(new GetAllTeamQuery(), CancellationToken.None);
+
+        _mapperMock.Verify(
+            m => m.Map<IEnumerable<TeamMemberDTO>>(teams), Times.Once);
+    }
+}
